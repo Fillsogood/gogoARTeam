cc9e2bd676f86cf2a433ce28a915b280b0817100
Fillsogood <94848819+Fillsogood@users.noreply.github.com>
2022-03-29 23:23:28 +0900

    r

---
+++ Assets/scripts/FollowUser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//script to make secondary camera follow user.
+public class FollowUser : MonoBehaviour
+{
+    public Transform userCamera;
+    public Text feedbacktext;
+    private void LateUpdate()
+    {
+        Vector3 newPosition = userCamera.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+        //  transform.eulerAngles = new Vector3(userCamera.rotation.x, 0, userCamera.rotation.z);
+        Vector3 rot = userCamera.eulerAngles;
+      //  feedbacktext.text = "rot x:" + rot.x + " ||y:" + rot.y + "||z:" + rot.z;
+        transform.eulerAngles = new Vector3(90, 0,- rot.y);
+    // transform.rotation = Quaternion.Euler(userCamera.rotation.x, userCamera.rotation.y, userCamera.rotation.z);
+    }
+}
---
+++ Assets/scripts/ViewSwitch.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewSwitch : MonoBehaviour
+{
+    public GameObject currentbuilding;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void EnableExternalModel()
+    {
+        // FeedbackText.text = "Enable ";
+        if (currentbuilding != null)
+        {
+            foreach (Transform child in currentbuilding.transform)
+            {
+                if (child.gameObject.name == "rebar")
+                {
+                    // FeedbackText.text += "rebar ";
+                    child.gameObject.SetActive(false);
+                }
+
+                if (child.gameObject.name == "external")
+                {
+                    //  FeedbackText.text += "external ";
+                    child.gameObject.SetActive(true);
+                }
+
+            }
+        }
+    }
+
+
+    public void DisableExternalModel()
+    {
+        // FeedbackText.text = "Disable ";
+        if (currentbuilding != null)
+        {
+            foreach (Transform child in currentbuilding.transform)
+            {
+
+                if (child.gameObject.name == "external")
+                {
+                    //  FeedbackText.text += "external ";
+                    child.gameObject.SetActive(false);
+                }
+
+                if (child.gameObject.name == "rebar")
+                {
+                    //  FeedbackText.text += "rebar ";
+                    child.gameObject.SetActive(true);
+                }
+
+
+
+            }
+        }
+    }
+}
